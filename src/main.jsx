73203be5ba799a9dcbbfe14a935edf1cc5fc69af
Daniel <cesar4herrera@gmail.com>
2024-07-15 16:48:33 -0600

    export const Props = () => {

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,14 @@
 import '@mantine/core/styles.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'; // Correct import for default export
+// import App from './App'; // Correct import for default export
+import { Props } from "./utils/props";
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Props />
   </React.StrictMode>
 );
