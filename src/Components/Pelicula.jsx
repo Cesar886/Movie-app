d26258117541971f6277862b3b3ef65c30d0156d
Daniel <cesar4herrera@gmail.com>
2024-07-16 14:16:34 -0600

    modulles

--- src/Components/Pelicula.jsx
+++ src/Components/Pelicula.jsx
@@ -1,28 +1,86 @@
-import { Badge, Card, Group, Image, MantineProvider, SimpleGrid, Text } from '@mantine/core'
+import { Container, SimpleGrid } from "@mantine/core";
+import { useEffect, useState } from "react";
+import { AddCategory } from "./AddCategory";
+import { GifItem } from "./GifItem";
 
 
-export const Pelicula = ({ title, url, id, desc, rate }) => {
+
+export default function Pelicula() {
+    const [categories, setCategories] = useState([' ']);
+    const [popularMovies, setPopularMovies] = useState([]);
+    const [searchResults, setSearchResults] = useState([]);
+    const [isSearching, setIsSearching] = useState(false);
+  
+    // const [opened, { open, close }] = useDisclosure(false);
+  
+  
+    const apiKey = 'abf7d734dcd7cce557ecf0abc3a863bf';
+    const popularUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
     
-  return (
-    <MantineProvider>
-      <SimpleGrid cols={1} spacing="lg">
-        <Card shadow="sm" padding="lg" radius="md" withBorder>
-          <Card.Section component="a">
-            <Image src={url} height={450} alt={title} />
-          </Card.Section>
-
-          <Group justify="space-between" mt="md" mb="xs">
-            <Text weight={500}>{title}</Text>
-            <Badge color="blue">{rate}</Badge>
-          </Group>
-
-          <Text size="sm" color="dimmed">
-            {desc}
-          </Text>
-        </Card>
-      </SimpleGrid>
-    </MantineProvider>
-  )
-}
-
-
+    useEffect(() => { 
+      fetch(popularUrl)
+        .then(response => response.json())
+        .then(data => {
+          console.log("🚀 ~ useEffect ~ data:", data)
+          setPopularMovies(data.results);
+        })
+        .catch(error => {
+          console.error("Syntax Error ", error);
+        });
+    }, []);
+  
+    const fetchMovies = (query) => {
+      const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`;
+      
+      fetch(searchUrl)
+        .then(response => response.json())
+        .then(data => {
+          setSearchResults(data.results);
+          setIsSearching(true);
+        })
+        .catch(error => {
+          console.error("Syntax Error ", error);
+        });
+    };
+  
+    const onAddCategory = (onNewCategory) => {
+      if (categories.includes(onNewCategory)) return;
+      setCategories([onNewCategory, ...categories]);
+      fetchMovies(onNewCategory);
+    };
+  
+    return (
+      <Container>
+        <AddCategory
+          onNewCategory={(event) => onAddCategory(event)}
+        />
+  
+        <SimpleGrid cols={3} spacing="lg">
+          {!isSearching ? (
+            popularMovies.map((movie) => (
+              <GifItem
+                id={movie.id}
+                key={movie.id}
+                title={movie.title}
+                url={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                desc={trimText(movie.overview, 20)}
+                rate={movie.vote_average}
+              />
+            ))
+          ) : (
+            searchResults.map((movie) => (
+              <GifItem
+                key={movie.id}
+                id={movie.id}
+                title={movie.title}
+                url={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                desc={trimText(movie.overview, 5)}
+                rate={movie.vote_average}
+              />
+            ))
+          )}
+        </SimpleGrid>
+      </Container>
+    );
+  }
+  
