eb77698f73360ca5b8fb1ea9399710a4d2f7af5d
Daniel <cesar4herrera@gmail.com>
2024-07-17 13:56:05 -0600

    iD funcional

--- src/Components/Pelicula.jsx
+++ src/Components/Pelicula.jsx
@@ -10,9 +10,14 @@ export const Pelicula = ({ id }) => {
 
   const [movieData, setMovieData] = useState([]);
 
+  const pathname = window.location.pathname;
+  const iD = pathname.match(/\d+/g);
+  console.log("🚀 ~ Pelicula ~ iD:", iD)
+
+
   const fetchMovies = async () => {
     try {
-      const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`);
+      const response = await fetch(`https://api.themoviedb.org/3/movie/${iD}?api_key=${apiKey}`);
       const data = await response.json();
       setMovieData(data);
       console.log("🚀 ~ fetchMovies ~ data:", data);
