5cc14efd0273b8cd6f9c6f23daf34f74b02c691c
Daniel <cesar4herrera@gmail.com>
2024-07-17 11:47:35 -0600

    pelicula

--- src/Components/Pelicula.jsx
+++ src/Components/Pelicula.jsx
@@ -1,6 +1,8 @@
-import { useEffect, useState } from "react";
-import { Badge, Button, Card, Container, Group, Image, MantineProvider, RingProgress, SimpleGrid, Text } from "@mantine/core";
-import './pelicula.css';
+import { Container, Title, Button, Group, Text, List, ThemeIcon, rem } from '@mantine/core';
+import { IconCheck } from '@tabler/icons-react';
+// import image from './image.svg';
+import classes from './HeroBullets.module.css';
+import { useEffect, useState } from 'react';
 
 const apiKey = "abf7d734dcd7cce557ecf0abc3a863bf";
 
@@ -9,7 +11,7 @@ export const Pelicula = ({ id }) => {
 
   const fetchMovies = async () => {
     try {
-      const response = await fetch(`https://api.themoviedb.org/3/movie/157336?api_key=${apiKey}`);
+      const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`);
       const data = await response.json();
       setMovieData(data);
       console.log("🚀 ~ fetchMovies ~ data:", data);
@@ -21,45 +23,55 @@ export const Pelicula = ({ id }) => {
   useEffect(() => {
     fetchMovies();
   }, [id]);
-
+  
+  
   return (
-    <Container>
-      {movieData ? (
-        <Card
-          key={movieData.id}
-          shadow="sm"
-          padding="xl"
-          radius="xl"
-        >
-          <Card.Section component="a">
-            <Image 
-              src={`https://image.tmdb.org/t/p/w500${movieData.poster_path}`} 
-              height={450} 
-              alt={movieData.title} 
-            />
-          </Card.Section>
-
-          <Group position="apart" mt="md" mb="xs">
-            <Text weight={500}>{movieData.title}</Text>
-            <RingProgress
-              sections={[{ value: movieData.vote_average * 10, color: 'blue' }]}
-              size={70}
-              thickness={6}
-              label={
-                <Text color="blue" weight={700} align="center" size={12}>
-                  {movieData.vote_average}
-                </Text>
-              }
-            />
-          </Group>
-
-          <Text size="sm" color="dimmed">
-            {movieData.overview}
+    <Container size="md">
+      <div className={classes.inner}>
+        <div className={classes.content}>
+          <Title className={classes.title}>
+            A <span className={classes.highlight}>modern</span> React <br /> components library
+          </Title>
+          <Text c="dimmed" mt="md">
+            uild fully functional accessible web applications faster than ever – Mantine includes
+            more than 120 customizable components and hooks to cover you in any situation
           </Text>
-        </Card>
-      ) : (
-        <Text>Cargando...</Text>
-      )}
+
+          <List
+            mt={30}
+            spacing="sm"
+            size="sm"
+            icon={
+              <ThemeIcon size={20} radius="xl">
+                <IconCheck style={{ width: rem(12), height: rem(12) }} stroke={1.5} />
+              </ThemeIcon>
+            }
+          >
+            <List.Item>
+              <b>TypeScript based</b> – build type safe applications, all components and hooks
+              export types
+            </List.Item>
+            <List.Item>
+              <b>Free and open source</b> – all packages have MIT license, you can use Mantine in
+              any project
+            </List.Item>
+            <List.Item>
+              <b>No annoying focus ring</b> – focus ring will appear only when user navigates with
+              keyboard
+            </List.Item>
+          </List>
+
+          <Group mt={30}>
+            <Button radius="xl" size="md" className={classes.control}>
+              Get started
+            </Button>
+            <Button variant="default" radius="xl" size="md" className={classes.control}>
+              Source code
+            </Button>
+          </Group>
+        </div>
+        {/* <Image src={image.src} className={classes.image} /> */}
+      </div>
     </Container>
   );
-};
+}
